f316373818eb8db055f165f5340c50da4e4025a9
itj-7 <ba.baghdadi@ensta.edu.dz>
2025-09-02 18:46:12 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+      window.location.href = 'yes.html'; 
+    });
+
+document.getElementById('noBtn').addEventListener('click', function() {
+      window.location.href = 'no.html'; 
+    });
